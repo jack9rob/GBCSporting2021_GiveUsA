d636529434d4b4cb2828b6725ec4a6f769a0eb61
jack9rob <jack9robinson@gmail.com>
2021-02-02 17:24:27 -0500

    method folder diappeared, had to add empty classes

---
+++ comp2139-assignment1/comp2139-assignment1/Models/Technician.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace comp2139_assignment1.Models
+{
+    public class Technician
+    {
+    }
+}
